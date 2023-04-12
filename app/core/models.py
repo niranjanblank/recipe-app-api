1088291161ce2a163187cf0bbc53361c26dd4e50
niranjan <niranjanshah474@gmail.com>
2023-04-12 18:15:35 +1000

    Linting fixed

--- app/core/models.py
+++ app/core/models.py
@@ -25,12 +25,13 @@ class UserManager(BaseUserManager):
 
     def create_superuser(self, email, password):
         "Create and return a new superuser"
-        user = self.create_user(email,password)
+        user = self.create_user(email, password)
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
 
         return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """User in the system"""
